f46c7b88e339d584d6366d17223f084dd7ffa13d
priyonto <priyontoo@gmail.com>
2022-03-28 13:53:09 +0200

    Add api for manual prediction via user input

--- app/main.py
+++ app/main.py
@@ -7,8 +7,27 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import pickle
 from datetime import date
-
 from starlette.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+class Features(BaseModel):
+    country_id: float
+    alt_nucl_en_perc: float
+    air_trans_freight: float
+    comb_ren_waste_perc: float
+    elec_prod_coal_perc: float
+    elec_prod_hydro_perc: float
+    elec_prod_nat_gas_perc: float
+    elec_prod_oil_perc: float
+    en_use_pc: float
+    fossil_fuel_en_cons_perc: float
+    gdp_pc: float
+    pop_growth_perc: float
+    pop_urban_agg: float
+    urban_pop_total: float
+    urban_pop_growth: float
+    methane_em: float
+    no2_em: float
 
 
 app = FastAPI()
@@ -54,24 +73,7 @@ async def getPrediction(country_id: int = 153, year: int = 2030):
     features_stream = open(os.path.abspath(features_filepath), "r")
     features_list = json.load(features_stream)
 
-    features = [ 
-        'ALT_NUCL_EN_PERC',
-        'AIR_TRANS_FREIGHT',
-        'COMB_REN_WASTE_PERC',
-        'ELEC_PROD_COAL_PERC',
-        'ELEC_PROD_HYDRO_PERC',
-        'ELEC_PROD_NAT_GAS_PERC',
-        'ELEC_PROD_OIL_PERC',
-        'EN_USE_PC',
-        'FOSSIL_FUEL_EN_CONS_PERC',
-        'GDP_PC',
-        'POP_GROWTH_PERC',
-        'POP_URBAN_AGG',
-        'URBAN_POP_TOTAL',
-        'URBAN_POP_GROWTH',
-        'METHANE_EM',
-        'NO2_EM'
-    ]
+    features = list(features_list.keys())
 
     # creating the date object of today's date
     todays_date = date.today()
@@ -105,4 +107,22 @@ async def getPrediction(country_id: int = 153, year: int = 2030):
     return {
         "country_id": country_id,
         "predictions": prediction_data
+    }
+
+@app.get("/api/v1/prediction-manual")
+async def getManualPrediction(input_features: Features):
+
+    features_dict = dict(input_features)
+    input_feature_values = list(features_dict.values())
+
+    # load the model from disk
+    model_name = "co2_emission_rf.pkl"
+    model_path = os.path.abspath(model_name)
+    model = pickle.load(open(model_path, 'rb'))
+
+    # Get prediction from model
+    prediction = model.predict([input_feature_values])[0]
+
+    return {
+        "co2_per_capita": prediction
     }
